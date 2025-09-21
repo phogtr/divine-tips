93a3386c9890067aa9d9981884757df47aeaba0d
phogbtr <phogbtr@gmail.com>
2025-09-21 12:58:16 -0500

    chg(event): event table schema, add event struct type
    - update event table schema - desc varchar -> data jsonb
    - add Event struct type

--- divine-server/services/event/type.go
+++ divine-server/services/event/type.go
@@ -1,5 +1,10 @@
 package event
 
+type Event struct {
+	Day  int          `json:"day"`
+	Data []*EventItem `json:"data"`
+}
+
 type EventItem struct {
 	Name            []string `json:"name"`
 	Type            int      `json:"type"`
