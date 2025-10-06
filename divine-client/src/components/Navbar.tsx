aedf4546e1353a6479e0d59121da21ade4d1be34
phogbtr <phogbtr@gmail.com>
2025-10-05 21:50:27 -0500

    dev: mock navbar link

--- divine-client/src/components/Navbar.tsx
+++ divine-client/src/components/Navbar.tsx
@@ -1,7 +1,27 @@
+import Link from "next/link";
+
 export const Navbar = () => {
   return (
-    <div>
-      <h2>Navbar</h2>
-    </div>
+    <nav className="flex justify-between items-center h-[50px] px-2 border border-white">
+      <div>
+        <Link href={"/"}>
+          <h2 className="text-xl">Divide Tips</h2>
+        </Link>
+      </div>
+      <div className="flex gap-5">
+        <Link href={"/"} className="text-lg">
+          Market
+        </Link>
+        <Link href={"/event"} className="text-lg">
+          Event
+        </Link>
+        <Link href={"/info"} className="text-lg">
+          Info
+        </Link>
+        <Link href={"/profile"} className="text-lg">
+          Profile
+        </Link>
+      </div>
+    </nav>
   );
 };
