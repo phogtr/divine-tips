56475cad33037a04987c6b85196ad4b3fd6677e6
phogbtr <phogbtr@gmail.com>
2025-10-21 23:11:29 -0500

    dev: add mock dialog ui in ItemContainer

--- divine-client/src/components/ItemContainer.tsx
+++ divine-client/src/components/ItemContainer.tsx
@@ -1,6 +1,14 @@
 "use client";
 import { useEffect, useState } from "react";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
 import { SideItems } from "./SideItems";
 import { ItemCard } from "./ItemCard";
 
@@ -13,6 +21,9 @@ interface ItemContainerProps {
 export const ItemContainer: React.FC<ItemContainerProps> = ({
   itemApiData,
 }) => {
+  const [openDialog, setOpenDialog] = useState(true);
+  const [input, setInput] = useState("");
+
   const [renderMap, setRenderMap] = useState<Record<number, boolean>>(() =>
     itemApiData.reduce(
       (acc, val) => ({
@@ -22,7 +33,6 @@ export const ItemContainer: React.FC<ItemContainerProps> = ({
       {}
     )
   );
-
   const [isToggleAll, setIsToggleAll] = useState(false);
 
   const onClickToggleItem = (id: number) => {
@@ -65,6 +75,51 @@ export const ItemContainer: React.FC<ItemContainerProps> = ({
       <main className="home-main-w overflow-auto">
         <ItemCard itemApiData={itemApiData} renderMap={renderMap} />
       </main>
+
+      <Dialog open={openDialog} onOpenChange={setOpenDialog}>
+        <DialogTrigger />
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle className="text-center">
+              Welcome to Divine Tips!
+            </DialogTitle>
+          </DialogHeader>
+
+          <div>
+            <p className="text-sm my-2">Let's start by entering username</p>
+
+            <input
+              type="text"
+              placeholder="Username"
+              className="border border-white rounded w-full h-10 p-2 mb-5"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+
+            <p className="text-sm">
+              Optionally, you can close this and continue without username
+            </p>
+            <p className="text-sm">But some features will be hidden</p>
+            <p className="text-sm mb-3.5">
+              Navigate to <b>Profile</b> at the top to open this again
+            </p>
+
+            <p className="text-sm mb-5">Have fun!</p>
+
+            <div className="text-center">
+              <button
+                className="border border-white rounded-[5px] w-[80px] h-[40px] cursor-pointer disabled:cursor-not-allowed disabled:border-gray-600"
+                onClick={() => {
+                  setOpenDialog(false);
+                }}
+                disabled={input === ""}
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+        </DialogContent>
+      </Dialog>
     </>
   );
 };
