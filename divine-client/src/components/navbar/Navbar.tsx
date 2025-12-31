947d1702ce317075368ee5dc81a2a898daa82a2c
phogbtr <phogbtr@gmail.com>
2025-12-30 21:56:56 -0600

    feat: navbar underline style

--- divine-client/src/components/navbar/Navbar.tsx
+++ divine-client/src/components/navbar/Navbar.tsx
@@ -5,6 +5,9 @@ import { ThemeToggle } from "../theme/ThemeToggle";
 import { UserBalance } from "./UserBalance";
 
 export const Navbar = () => {
+  const linkUnderlineClass =
+    "h-0.5 w-0 bg-white duration-400 ease-in-out group-hover:w-full";
+
   return (
     <nav className="h-(--navbar-h) flex items-center px-2 border border-white">
       <div className="flex w-full grow-1 shrink-2 justify-start">
@@ -18,17 +21,21 @@ export const Navbar = () => {
       </div>
 
       <div className="flex w-full grow-1 shrink-2 justify-end gap-5">
-        <Link href={"/"} className="text-base">
+        <Link href={"/"} className="group text-base">
           Market
+          <div className={linkUnderlineClass} />
         </Link>
-        <Link href={"/event"} className="text-base">
+        <Link href={"/event"} className="group text-base">
           Event
+          <div className={linkUnderlineClass} />
         </Link>
-        <Link href={"/info"} className="text-base">
+        <Link href={"/info"} className="group text-base">
           Info
+          <div className={linkUnderlineClass} />
         </Link>
-        <Link href={"/profile"} className="text-base">
+        <Link href={"/profile"} className="group text-base">
           Profile
+          <div className={linkUnderlineClass} />
         </Link>
 
         <ThemeToggle />
